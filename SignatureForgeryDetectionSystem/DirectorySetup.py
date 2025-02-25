cd74a14616a05aebc61f9089f03a872ad63ff300
Pasan Diksura <pasan.20210415@iit.ac.lk>
2025-02-26 00:09:58 +0530

    Create DirectorySetup.py

---
+++ SignatureForgeryDetectionSystem/DirectorySetup.py
@@ -0,0 +1,38 @@
+import os
+import subprocess
+
+## ===============================================================================================================
+## MAKE SURE YOU HAVE SETUPED THE VIRTUAL ENVIROMENT BEFORE RUN THIS, IF NOT IT'LL INSTALL PACKAGES GLOBALLY.
+## ===============================================================================================================
+
+## -----------------------------------------------------------------------------------------------------------
+## Packages Installations
+subprocess.run(["pip", "install", "-r", "requirements.txt"], check=True)
+
+
+## -----------------------------------------------------------------------------------------------------------
+## Directory Setup
+main_path = "./SignatureForgeryDetectionSystem/MachineLearning/Preprocessing/"
+
+os.makedirs(os.path.join(main_path, "Dataset"), exist_ok=True)
+
+
+datasetDir_path = "./SignatureForgeryDetectionSystem/MachineLearning/Preprocessing/Dataset/"
+
+os.makedirs(os.path.join(datasetDir_path, "Features"), exist_ok=True)
+os.makedirs(os.path.join(datasetDir_path, "Processing"), exist_ok=True)
+os.makedirs(os.path.join(datasetDir_path, "Resources"), exist_ok=True)
+
+
+processingDir_path = "./SignatureForgeryDetectionSystem/MachineLearning/Preprocessing/Dataset/Processing/"
+
+os.makedirs(os.path.join(processingDir_path, "AdvanceTestingDataset"), exist_ok=True)
+os.makedirs(os.path.join(processingDir_path, "AugmentedDataset"), exist_ok=True)
+os.makedirs(os.path.join(processingDir_path, "HybridDataset"), exist_ok=True)
+os.makedirs(os.path.join(processingDir_path, "Pre-ProcessedDataset"), exist_ok=True)
+
+
+resourcesDir_path = "./SignatureForgeryDetectionSystem/MachineLearning/Preprocessing/Dataset/Resources/"
+
+os.makedirs(os.path.join(resourcesDir_path, "Backup Dataset"), exist_ok=True)
+os.makedirs(os.path.join(resourcesDir_path, "ProcessingDataset"), exist_ok=True)
