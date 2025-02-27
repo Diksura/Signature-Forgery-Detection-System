9eea846da9b8cfacda306d59333bcc8ce66b8691
Pasan Diksura <pasan.20210415@iit.ac.lk>
2025-02-27 05:40:57 +0530

    Update DirectorySetup.py

--- SignatureForgeryDetectionSystem/DirectorySetup.py
+++ SignatureForgeryDetectionSystem/DirectorySetup.py
@@ -12,19 +12,20 @@ subprocess.run(["pip", "install", "-r", "./SignatureForgeryDetectionSystem/requi
 
 ## -----------------------------------------------------------------------------------------------------------
 ## Directory Setup
-main_path = "./SignatureForgeryDetectionSystem/MachineLearning/Preprocessing/"
+main_path = "./SignatureForgeryDetectionSystem/MachineLearning/"
 
 os.makedirs(os.path.join(main_path, "Dataset"), exist_ok=True)
+os.makedirs(os.path.join(main_path, "Model"), exist_ok=True)
 
 
-datasetDir_path = "./SignatureForgeryDetectionSystem/MachineLearning/Preprocessing/Dataset/"
+datasetDir_path = "./SignatureForgeryDetectionSystem/MachineLearning/Dataset/"
 
 os.makedirs(os.path.join(datasetDir_path, "Features"), exist_ok=True)
 os.makedirs(os.path.join(datasetDir_path, "Processing"), exist_ok=True)
 os.makedirs(os.path.join(datasetDir_path, "Resources"), exist_ok=True)
 
 
-processingDir_path = "./SignatureForgeryDetectionSystem/MachineLearning/Preprocessing/Dataset/Processing/"
+processingDir_path = "./SignatureForgeryDetectionSystem/MachineLearning/Dataset/Processing/"
 
 os.makedirs(os.path.join(processingDir_path, "AdvanceTestingDataset"), exist_ok=True)
 os.makedirs(os.path.join(processingDir_path, "AugmentedDataset"), exist_ok=True)
@@ -32,7 +33,7 @@ os.makedirs(os.path.join(processingDir_path, "HybridDataset"), exist_ok=True)
 os.makedirs(os.path.join(processingDir_path, "Pre-ProcessedDataset"), exist_ok=True)
 
 
-resourcesDir_path = "./SignatureForgeryDetectionSystem/MachineLearning/Preprocessing/Dataset/Resources/"
+resourcesDir_path = "./SignatureForgeryDetectionSystem/MachineLearning/Dataset/Resources/"
 
 os.makedirs(os.path.join(resourcesDir_path, "Backup Dataset"), exist_ok=True)
 os.makedirs(os.path.join(resourcesDir_path, "ProcessingDataset"), exist_ok=True)
